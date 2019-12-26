e5e74c79f9583d65753e0cd46b781046e2a76533
devlopix <othmane.devlopix@gmail.com>
2019-12-26 14:50:50 +0100

    Modif message

--- src/main/java/ma/gr/sample_project/App.java
+++ src/main/java/ma/gr/sample_project/App.java
@@ -6,6 +6,6 @@ package ma.gr.sample_project;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello Maven & Jenkins !");
+		System.out.println("Hello Maven & Jenkins 1 !");
 	}
 }
